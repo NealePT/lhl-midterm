828114c4ee500139db58c60b757136929b786f3e
mrjohnming <john.ngai@hotmail.com>
2022-02-12 14:58:51 +0000

    Minor updates

--- routes/users2.js
+++ routes/users2.js
@@ -6,6 +6,7 @@ const router  = express.Router();
 
 // 5. GET /users/:id - The end-user wants to see the collection for a specific profile.
 router.get('/users/:id', (req, res) => {
+  // REMINDER: Need to replace with users_index before merging with -b master.
   res.render('temp_users_index');
 });
 
