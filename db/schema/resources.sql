40937d0fa4be4cd6b7d0344b55fab8bd2c644b97
Neale Taylor <nealeptaylor@gmail.com>
2022-02-12 14:43:07 -0500

    add seeds and schema for test users

---
+++ db/schema/resources.sql
@@ -0,0 +1,23 @@
+-- ## resources
+
+-- * id
+-- * owner_id
+-- * title
+-- * description
+-- * category
+-- * url
+-- * date_created
+-- * date_modified
+
+
+DROP TABLE IF EXISTS resources CASCADE;
+CREATE TABLE resources (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  category VARCHAR(255) NOT NULL,
+  url VARCHAR(1024) NOT NULL,
+  date_created DATE,
+  date_modified DATE DEFAULT NULL
+)
