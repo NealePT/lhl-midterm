ae93850e11c8944ee1c80f32a247e791d0660525
mrjohnming <john.ngai@hotmail.com>
2022-02-13 15:33:11 +0000

    Add more fake data

--- db/seeds/resources.sql
+++ db/seeds/resources.sql
@@ -3,4 +3,6 @@ VALUES (1, 'ITS FRIDAY THEN, ITS SATURDAY, SUNDAY! GO MUFASA!', 'TGIF everyone!!
 (2, 'Flexbox Froggy', 'A game to help you learn CSS Flexbox.', 'tutorial', 'https://flexboxfroggy.com/', '2015-11-25'),
 (3, 'Scarlatti Sonata in E Major, K.380', 'Peformed by classical concert pianist, Tiffany Poon.', 'video', 'https://www.youtube.com/watch?v=EcmFCpoTKXc', '2019-12-13'),
 (2, 'I spent my vacation programming', 'I was just on vacation for four weeks and I spent the entire time writing code.', 'blog', 'https://codeutopia.net/blog/2017/07/11/i-spent-my-vacation-programming/', '2017-03-14'),
-(3, 'Music to put you in a better mood ~ lofi / relax / stress relief', 'Little Soul on Youtube', 'video', 'https://www.youtube.com/watch?v=NiOxSuEJaTI', '2021-07-19');
+(3, 'Music to put you in a better mood ~ lofi / relax / stress relief', 'Little Soul on Youtube', 'video', 'https://www.youtube.com/watch?v=NiOxSuEJaTI', '2021-07-19'),
+(1, 'Tessa Virtue and Scott Moir''s Moulin Rouge at PyeongChang 2018', '
+Tessa and Scott have had an extraordinary career as the most decorated figure skaters of all time', 'video', 'https://www.youtube.com/watch?v=wOEKdWrtz6U', '2018-03-05');
