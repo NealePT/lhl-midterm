5b27c03df63f3d63813415e01cad5e92cb228911
mrjohnming <john.ngai@hotmail.com>
2022-02-13 21:00:44 +0000

    Add a Guest user for testing

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,4 +2,5 @@
 INSERT INTO users (name, email, password)
 VALUES ('Neale Taylor', 'nealetaylor@neale.com', '123'),
 ('Jerry Sanderson', 'jerrysandy@jerry.com', '456'),
-('Terry Sanderson', 'terrysandy@terry.com', '789');
+('Terry Sanderson', 'terrysandy@terry.com', '789'),
+('Guest', 'guest@gmail.com', 'password');
