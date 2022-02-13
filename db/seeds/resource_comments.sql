1256e0ad8471dbf15dc403ec48659c1c4e426bfb
mrjohnming <john.ngai@hotmail.com>
2022-02-13 06:00:15 +0000

    Create more fake data

---
+++ db/seeds/resource_comments.sql
@@ -0,0 +1,3 @@
+INSERT INTO resource_comments (owner_id, resource_id, comment)
+VALUES (2, 1, 'It''s not Friday in our family until we watch this at least twice!'),
+(3, 1, 'This is so positive. The music is up. The brightly colored clothes. Thanks Mufasa!');
--- db/seeds/resources.sql
+++ db/seeds/resources.sql
@@ -1,3 +1,6 @@
 INSERT INTO resources (owner_id, title, description, category, url, date_created)
-VALUES (1, 'title1', 'description1', 'blog', 'https://www.thegeekstuff.com/2012/04/curl-examples/', '2022-02-12'),
-(2, 'title2', 'description2', 'video', 'https://www.youtube.com/watch?v=tYxI9CV2Zyk', '2022-02-12')
+VALUES (1, 'ITS FRIDAY THEN, ITS SATURDAY, SUNDAY! GO MUFASA!', 'TGIF everyone!!', 'video', 'https://www.youtube.com/watch?v=1TewCPi92ro', '2019-12-18'),
+(2, 'Flexbox Froggy', 'A game to help you learn CSS Flexbox.', 'tutorial', 'https://flexboxfroggy.com/', '2015-11-25'),
+(3, 'Scarlatti Sonata in E Major, K.380', 'Peformed by classical concert pianist, Tiffany Poon.', 'video', 'https://www.youtube.com/watch?v=EcmFCpoTKXc', '2019-12-13'),
+(2, 'I spent my vacation programming', 'I was just on vacation for four weeks and I spent the entire time writing code.', 'blog', 'https://codeutopia.net/blog/2017/07/11/i-spent-my-vacation-programming/', '2017-03-14'),
+(3, 'Music to put you in a better mood ~ lofi / relax / stress relief', 'Little Soul on Youtube', 'video', 'https://www.youtube.com/watch?v=NiOxSuEJaTI', '2021-07-19');
