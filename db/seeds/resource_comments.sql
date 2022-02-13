bf076a99917c050cf3a681c42637e883cf6b0d5b
mrjohnming <john.ngai@hotmail.com>
2022-02-13 21:19:55 +0000

    Add date entity

--- db/seeds/resource_comments.sql
+++ db/seeds/resource_comments.sql
@@ -1,3 +1,5 @@
-INSERT INTO resource_comments (owner_id, resource_id, comment)
-VALUES (2, 1, 'It''s not Friday in our family until we watch this at least twice!'),
-(3, 1, 'This is so positive. The music is up. The brightly colored clothes. Thanks Mufasa!');
+
+-- TIMESTAMP YYYY-MM-DD HH:MI:SS
+INSERT INTO resource_comments (owner_id, resource_id, date, comment)
+VALUES (2, 1, '2021-11-11 13:23:44', 'It''s not Friday in our family until we watch this at least twice!'),
+(3, 1, '2022-01-09 15:45:21', 'This is so positive. The music is up. The brightly colored clothes. Thanks Mufasa!');
