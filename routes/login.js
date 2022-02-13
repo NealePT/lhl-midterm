b7fb13e804a9ff04bb659068071b08b264661cb2
Vivian Zhang <vvynz12@gmail.com>
2022-02-13 00:41:56 +0000

    update getUserWithEmail function to accept 2 parameters.

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,11 @@
 // Dependencies
 const express = require('express');
 const router = express.Router();
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
 
 
 // 7. GET /login - The end-user wants to log into their account.
@@ -10,23 +15,30 @@ router.get('/login', (req, res) => {
   res.render('temp_login');
 });
 
-const getUserWithEmail = function(email) {
-  const queryString = `SELECT * FROM users WHERE email = $1`;
-
-  return db.query(queryString, [email])
+const getUserWithEmail = function(email password) {
+  const queryString = `SELECT * FROM users WHERE email = $1 AND password = $2`;
+  const values = [user.email, user.password];
+  return db.query(queryString, values)
     .then((res) => {
       console.log(res.rows);
-      // return res.rows;
+      return res.rows;
     })
     .catch((err) => {
       return console.log(err.message);
     })
 }
 
-exports.getUserWithEmail = getUserWithEmail;
+// exports.getUserWithEmail = getUserWithEmail;
 
 router.post('/login', (req, res) => {
   console.log(req.body);
+  const userEmail = req.body.email;
+
+  // if no email is entered
+  if (!userEmail) {
+    return console.log("💩");
+  }
+
   res.render('temp_collections_index');
 })
 module.exports = router;
