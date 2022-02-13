68ab344ff63a41515af4a447dc936ebfe6c651b5
mrjohnming <john.ngai@hotmail.com>
2022-02-13 05:14:13 +0000

    SQL queries

---
+++ database.js
@@ -0,0 +1,16 @@
+// Dependencies
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+
+const getResourceDetails = resourceID => {
+  const values = [resourceID];
+  const query = `
+  SELECT * FROM resources
+  JOIN users ON users.id = owner_id
+  WHERE resources.id = $1;
+  `;
+  return db.query(query, values)
+  .then(res => res.rows[0]);
+};
+exports.getResourceDetails = getResourceDetails;
