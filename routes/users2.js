f6d4993394cc9c65620d90eda6b01ba9f912a18d
Vivian Zhang <vvynz12@gmail.com>
2022-02-15 15:41:46 +0000

    minor updates

--- routes/users2.js
+++ routes/users2.js
@@ -5,20 +5,20 @@ const express = require('express');
 const router = express.Router();
 const database = require('../database');
 
+// PG database client/connection setup
+const { Pool } = require("pg");
+const dbParams = require("../lib/db.js");
+const db = new Pool(dbParams);
+
 // 5. GET /users/:id - The end-user wants to see the collection for a specific profile.
 router.get('/users/:id', (req, res) => {
   // REMINDER: Need to replace with users_index.
 
-  const resourcesID = req.params.id;
-  console.log(resourceID);
-  const userID = 4; //id = 4 is a guest id (for testing)
-  const resParams = { resourceID: resourcesID };
-  database.getResourceDetails(resourceID)
-    .then(data => {
-      console.log("DATA:", data);
-      resParams.id = data.id;
-    })
-    .then(() => res.render('temp_users_index', resParams));
+  const resourceID = req.params.id;
+  console.log("PARAMS", req.params.id);
+  const userID = 1; //id = 4 is a guest id (for testing)
+  const resParams = { resourceID: resourceID, userID: userID };
+  res.render('temp_users_index', resParams);
 });
 
 module.exports = router;
