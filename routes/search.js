d24429d02d1c7a4e31e14a9233f430b0decfe29c
mrjohnming <john.ngai@hotmail.com>
2022-02-15 17:11:37 +0000

    Add GET /search/:id

--- routes/search.js
+++ routes/search.js
@@ -1,11 +1,24 @@
 // Dependencies
 const express = require('express');
 const router  = express.Router();
+const database = require('../database'); // Contains all SQL query functions.
 
 // 8. GET /search/:id - The end-user wants to find a resource using a specific search phrase.
 router.get('/search/:id', (req, res) => {
-  // REMINDER: Need to replace with search.ejs.
-  res.render('temp_search');
+  const searchPhrase = req.params.id;
+  const resParams = {};
+
+  database.getSearchResults(searchPhrase)
+  .then(data => {
+    resParams.searchResults = data;
+  })
+  .then(() => res.render('temp_search', resParams));
+});
+
+// POST /search
+router.post('/search', (req, res) => {
+  const searchPhrase = req.body['search-phrase'];
+  res.redirect(`/search/${searchPhrase}`);
 });
 
 module.exports = router;
