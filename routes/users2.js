77d6f3fc9c60b0766b4f20798a1c91a3505cac4a
Vivian Zhang <vvynz12@gmail.com>
2022-02-15 01:20:31 +0000

    Update edit buttons to redirect to a user's update page for a resource in their database. Currently still just loads instead of redirecting

--- routes/users2.js
+++ routes/users2.js
@@ -2,12 +2,17 @@
 
 // Dependencies
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 // 5. GET /users/:id - The end-user wants to see the collection for a specific profile.
 router.get('/users/:id', (req, res) => {
   // REMINDER: Need to replace with users_index.
-  res.render('temp_users_index');
+
+  const resourceID = req.params.id;
+  console.log(resourceID);
+  const userID = 1; //id = 4 is a guest id (for testing)
+  const resParams = { resourceID: resourceID, userID: userID };
+  res.render('temp_users_index', resParams);
 });
 
 module.exports = router;
