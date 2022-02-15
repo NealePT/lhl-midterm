13df3811ca501e22bc87b70c1ad969efcfa41897
Vivian Zhang <vvynz12@gmail.com>
2022-02-15 18:53:10 +0000

    Obtain resource data in the GET route fromour databases.

--- routes/users2.js
+++ routes/users2.js
@@ -6,9 +6,9 @@ const router = express.Router();
 const database = require('../database'); //contains all SQL query functions
 
 // PG database client/connection setup
-const { Pool } = require("pg");
-const dbParams = require("../lib/db.js");
-const db = new Pool(dbParams);
+// const { Pool } = require("pg");
+// const dbParams = require("../lib/db.js");
+// const db = new Pool(dbParams);
 
 // 5. GET /users/:id - The end-user wants to see the collection for a specific profile.
 router.get('/users/:id', (req, res) => {
@@ -18,7 +18,19 @@ router.get('/users/:id', (req, res) => {
   console.log("PARAMS", req.params.id);
   const userID = 4; //id = 4 is a guest id (for testing)
   const resParams = { resourceID: resourceID, userID: userID };
-  res.render('temp_users_index', resParams);
+
+  // get the resource details from the resources & user tables
+  database.getResourceDetails(resourceID)
+    .then(data => {
+      console.log("DATA", data);
+      resParams.name = data.name;
+      resParams.title = data.title;
+      resParams.description = data.description;
+      resParams.date_created = data.date_created;
+    })
+
+    // pass our resParams data and render the user's index page
+    .then(() => res.render('temp_users_index', resParams));
 });
 
 module.exports = router;
