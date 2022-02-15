9742011eaed6a836cec42f7dca424a4b72228fa4
mrjohnming <john.ngai@hotmail.com>
2022-02-15 01:41:55 +0000

    Add/update fake data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,24 @@
--- Users table seeds here (Example)
+-- Fake data
+-- password = $2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn
+
 INSERT INTO users (name, email, password)
-VALUES ('Neale Taylor', 'nealetaylor@neale.com', '123'),
-('Jerry Sanderson', 'jerrysandy@jerry.com', '456'),
-('Terry Sanderson', 'terrysandy@terry.com', '789'),
-('Guest', 'guest@gmail.com', 'password');
+VALUES ('Guest', 'guest@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Neale Taylor', 'ntaylor@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('John Ngai', 'jngai@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Vivian Zhang', 'vzhang@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Bob Marley', 'bmarley@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Frank Abagnale', 'fabagnale@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Ray Dalio', 'rdalio@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Elizabeth Hurley', 'ehurley@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Benedict Cumberbatch', 'bcumberbatch@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Elon Musk', 'elon@tesla.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Jeff Bezos', 'jeff@amazon.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Elizabeth Olsen', 'eolsen@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Christoph Waltz', 'cwaltz@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Jack Nicholson', 'jnicholson@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Elizabeth Windsor', 'queen@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Joe Biden', 'president@whitehouse.gov', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Andrew Huberman', 'adh1@stanford.edu', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Tony Robbins', 'tony@tonyrobbins.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('Sarah Gadon', 'sgadon@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn'),
+('John Krasinski', 'jrasinski@gmail.com', '$2b$10$cLKCIcK26YHzcp4rM8MucOdi37M7U3rr7qH/pV.TsJn')
