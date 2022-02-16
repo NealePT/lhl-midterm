4fdba1e93e873ecebd611ddccbd83b408936a043
Neale Taylor <nealeptaylor@gmail.com>
2022-02-15 19:12:29 -0500

    add more likes, comments, ratings to db

--- db/seeds/resource_comments.sql
+++ db/seeds/resource_comments.sql
@@ -1,3 +1,11 @@
 INSERT INTO resource_comments (owner_id, resource_id, date, comment)
 VALUES (2, 1, '2021-11-11 13:23:44', 'It''s not Friday in our family until we watch this at least twice!'),
-(3, 1, '2022-01-09 15:45:21', 'This is so positive. The music is up. The brightly colored clothes. Thanks Mufasa!');
+(3, 1, '2022-01-09 15:45:21', 'This is so positive. The music is up. The brightly colored clothes. Thanks Mufasa!'),
+(4, 2, '2022-01-09 17:12:45', 'Fun game that taught me all the flexbox Ill ever need!'),
+(5, 3, '2022-01-08 09:13:42', 'I never get tired of this piece!'),
+(5, 4, '2022-02-01 05:18:12', 'Sounds like quite the vacation.'),
+(6, 5, '2022-02-07 16:35:53', 'I dont know if I prefer this or lofi hiphop beats.'),
+(1, 6, '2022-01-07 17:11:53', ' Whoever choreographed this needs an Oscar '),
+(3, 6, '2022-01-04 13:02:54', 'It is beijing 2022 and i still watch this phenomenal performance and miss tessa and scott so much!!!'),
+(5, 7, '2022-01-04 13:02:54', 'I very much dislike SQLzoo'),
+(2, 7, '2022-02-03 1:32:56', 'SQLZoo? more like SQLZzz. Ha got em');
