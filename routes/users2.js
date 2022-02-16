996ab34eb4cd7e55f5d5c54990aed720dfb67756
Vivian Zhang <vvynz12@gmail.com>
2022-02-16 19:24:31 +0000

    Remove unused lines of code and console logs

--- routes/users2.js
+++ routes/users2.js
@@ -13,60 +13,34 @@ router.get('/users/:id', (req, res) => {
   const likesResourceID = resourceID;
   const userID = 1;
   const resParams = { resourceID: resourceID, likesResourceID: likesResourceID, userID: userID };
-  // console.log("resourceID", resourceID);
-  // console.log("LIKES", likesResourceID);
 
   // get the resource details from the user's resources
   database.getAllResources(resourceID)
     .then(data => {
       resParams.user_id = data[0].owner_id;
-      console.log("USERID:", resParams.user_id);
+      // console.log("USERID:", resParams.user_id);
     })
 
     .then(() => database.getAllResources(resourceID))
     .then(data => {
-      console.log("ALL resources", data);
+      // console.log("ALL resources", data);
       resParams.resourceID = data;
-      // resParams.allTitles = data.title;
-      // resParams.allDesc = data.description;
-      // resParams.allDate_Created = data.date_created;
     })
 
     .then(() => database.getAllLikedResources(likesResourceID))
     .then(data => {
       resParams.name = data[0].name;
-      console.log(resParams.name);
+      // console.log(resParams.name);
     })
     // get all of a user's liked resources
     .then(() => database.getAllLikedResources(likesResourceID))
     .then(data => {
       console.log("LIKED", data);
       resParams.likesResourceID = data;
-      // resParams.likedTitles = data.title;
-      // resParams.likedDesc = data.description;
-      // resParams.likedDate_Created = data.date_created;
     })
 
     //pass our resParams data and render the user's index page
     .then(() => res.render('temp_users_index', resParams));
-  console.log(resParams);
-
-
-
-  // database.getAllResources(resourceID)
-  //   .then(data => {
-  //     resParams.resourceID = data;
-  //     resParams.title = data[0].title;
-  //     console.log("TITLE", data[0].title);
-  //     console.log("DATA", data);
-  //   })
-
-  // database.getAllLikedResources(likesResourceID)
-  //   .then(data => {
-  //     // console.log("LIKED", data);
-  //     resParams.likesResourceID = data;
-  //   })
-  // .then(() => res.render('temp_users_index', resParams));
 });
 
 module.exports = router;
