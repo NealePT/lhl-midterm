f1dccf4c113907c162138f4211b906b66bbc7b40
Vivian Zhang <vvynz12@gmail.com>
2022-02-16 01:37:16 +0000

    update getAllLikedResources to return user's name

--- database.js
+++ database.js
@@ -191,7 +191,7 @@ exports.getAllResources = getAllResources;
 const getAllLikedResources = (resourceID) => {
   const value = [resourceID];
   const query = `
-  SELECT title, description, TO_CHAR(date_created, 'Mon dd, yyyy') AS date_created FROM resources
+  SELECT users.name, title, description, TO_CHAR(date_created, 'Mon dd, yyyy') AS date_created FROM resources
   JOIN resource_likes ON resource_id = resources.id
   JOIN users ON users.id = resources.owner_id
   WHERE resources.owner_id = $1;`;
