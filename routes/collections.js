6e7d42808946289fddb10aec991ce0f12ff93f1a
mrjohnming <john.ngai@hotmail.com>
2022-02-16 00:28:55 +0000

    Setup cookie-session for all routes

--- routes/collections.js
+++ routes/collections.js
@@ -22,6 +22,9 @@ router.get('/collections', (req, res) => {
       resParams.sessionID = sessionID;
     })
 
+    // REMINDER: Remove test code
+    .then(() => console.log('GET /collections =', resParams))
+
     // REMINDER: Need to eventually replace with collections_index.
     .then(() => res.render('temp_collections_index', resParams));
   }
@@ -45,6 +48,9 @@ router.get('/collections/new', (req, res) => {
       resParams.username = data.name;
       resParams.sessionID = sessionID;
     })
+
+    // REMINDER: Remove test code
+    .then(() => console.log('GET /collections/new =', resParams))
     .then(() => res.render('collections_new', resParams));
   }
 });
@@ -52,13 +58,9 @@ router.get('/collections/new', (req, res) => {
 
 // 3. GET /collections/:id - The end-user wants to see a particular resource.
 router.get('/collections/:id', (req, res) => {
-  const sessionID = req.session.user_id; // sessionID = userID
+  const sessionID = req.session.user_id;
   const resourcesID = req.params.id;
-  const resParams = {
-    resourceID: resourcesID,
-    userID: sessionID
-  };
-
+  const resParams = { resourceID: resourcesID };
 
   // Get most of the relevant properties from the resources & users tables.
   database.getResourceDetails(resourcesID)
@@ -68,6 +70,19 @@ router.get('/collections/:id', (req, res) => {
     resParams.category = data.category;
     resParams.url = data.url;
     resParams.publisher = data.name;
+  })
+
+  // If there is a session cookie, pass the cookie and matching user name before rendering the page.
+  .then(() => {
+    if (!sessionID) {
+      resParams.sessionID = null;
+    } else {
+      database.getNameByUserID(sessionID)
+      .then(data => {
+        resParams.username = data.name;
+        resParams.sessionID = sessionID;
+      });
+    }
   })
 
   // Get the two dates from the resources table.
@@ -97,6 +112,9 @@ router.get('/collections/:id', (req, res) => {
   .then(() => database.getComments(resourcesID))
   .then(data => resParams.comments = data)
 
+  // REMINDER: Remove test code
+  .then(() => console.log('GET /collections/:id =', resParams))
+
   // Pass in the relevant data (resParams) and render the page.
   .then(() => {res.render('collections_show', resParams)});
 });
@@ -104,35 +122,65 @@ router.get('/collections/:id', (req, res) => {
 
 // 4. GET /collections/:id/update - The end-user wants to update an existing resource.
 router.get('/collections/:id/update', (req, res) => {
+  const sessionID = req.session.user_id;
   const resourceID = req.params.id;
   const resParams = { resourceID: resourceID };
 
-  database.getResourceDetails(resourceID)
-  .then(data => {
-    resParams.title = data.title;
-    resParams.description = data.description;
-    resParams.category = data.category;
-    resParams.url = data.url;
-  })
-  .then(() => res.render('collections_update', resParams));
+  if (!sessionID) {
+    return res.status(404).send(`
+      Please <a href="/login">login</a> to view this page.
+    `);
+  } else {
+    database.getResourceDetails(resourceID)
+    .then(data => {
+      let publisherID = data['owner_id'];
+      console.log('publisherID =', publisherID);
+      console.log('sessionID =', sessionID);
+      if (publisherID !== sessionID) {
+        return res.status(403).send(`
+        Access Denied: This resource belongs to someone else.
+        `);
+      } else {
+        resParams.title = data.title;
+        resParams.description = data.description;
+        resParams.category = data.category;
+        resParams.url = data.url;
+      }
+    })
+    .then(() => database.getNameByUserID(sessionID))
+    .then(data => {
+      resParams.username = data.name;
+      resParams.sessionID = sessionID;
+    })
+
+    // REMINDER: Remove test code
+    .then(() => console.log('GET /collections/:id/update =', resParams))
+    .then(() => res.render('collections_update', resParams));
+  }
 });
 
 // POST /collections
 router.post('/collections', (req, res) => {
-  const ownerID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
+
+  // IMPORTANT: Add conditional to prevent access through curl unless logged in.
+
+  const sessionID = req.session.user_id;
   const title = req.body.title;
   const description = req.body.description;
   const url = req.body.url;
   const category = req.body.category;
   let resourceID;
 
-  database.addResource(ownerID, title, description, url, category)
+  database.addResource(sessionID, title, description, url, category)
   .then(data => resourceID = data.id)
   .then(() => res.redirect(`/collections/${resourceID}`));
 });
 
 // POST /collections/:id/update
 router.post('/collections/:id/update', (req, res) => {
+
+  // IMPORTANT: Add conditional to prevent access through curl unless logged in AND publisher.
+
   const resourceID = req.params.id;
   const newTitle = req.body.title;
   const newDescription = req.body.description;
@@ -145,6 +193,9 @@ router.post('/collections/:id/update', (req, res) => {
 
 // POST /collections/:id/delete
 router.post('/collections/:id/delete', (req, res) => {
+
+  // IMPORTANT: Add conditional to prevent access through curl unless logged in AND publisher.
+
   const userID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
   const resourceID = req.params.id;
 
@@ -154,39 +205,63 @@ router.post('/collections/:id/delete', (req, res) => {
 
 // POST /collections/:id/like
 router.post('/collections/:id/like', (req, res) => {
-  const ownerID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
+  const sessionID = req.session.user_id;
   const resourceID = req.params.id;
 
-  database.addLike(resourceID, ownerID)
+  if (!sessionID) {
+    return res.status(403).send(`
+    Please <a href="/login">login</a> to like this page.
+    `);
+  }
+
+  database.addLike(resourceID, sessionID)
   .then(() => res.redirect(`/collections/${resourceID}`));
 });
 
 // POST /collections/:id/unlike
 router.post('/collections/:id/unlike', (req, res) => {
-  const ownerID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
+  const sessionID = req.session.user_id;
   const resourceID = req.params.id;
 
-  database.removeLike(resourceID, ownerID)
+  if (!sessionID) {
+    return res.status(403).send(`
+    You must be <a href="/login">signed-in</a> to use this feature.
+    `);
+  }
+
+  database.removeLike(resourceID, sessionID)
   .then(() => res.redirect(`/collections/${resourceID}`));
 });
 
 // POST /collections/:id/rating
 router.post('/collections/:id/rating' ,(req, res) => {
-  const ownerID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
+  const sessionID = req.session.user_id;
   const resourceID = req.params.id;
-  const rating = Number(req.body.rating);
 
-  database.addRating(ownerID, resourceID, rating)
+  if (!sessionID) {
+    return res.status(403).send(`
+    Please <a href="/login">login</a> to rate this page.
+    `);
+  }
+
+  const rating = Number(req.body.rating);
+  database.addRating(sessionID, resourceID, rating)
   .then(() => res.redirect(`/collections/${resourceID}`));
 });
 
 // POST /collections/:id/comment
 router.post('/collections/:id/comment', (req, res) => {
-  const ownerID = 4; // Table users id = 4 is Guest (for testing only and not an actual Guest account)
+  const sessionID = req.session.user_id;
   const resourceID = req.params.id;
-  const comment = req.body['comment-text'];
 
-  database.addComment(ownerID, resourceID, comment)
+  if (!sessionID) {
+    return res.status(403).send(`
+    Please <a href="/login">login</a> to comment on this page.
+    `);
+  }
+
+  const comment = req.body['comment-text'];
+  database.addComment(sessionID, resourceID, comment)
   .then(() => res.redirect(`/collections/${resourceID}`));
 });
 
