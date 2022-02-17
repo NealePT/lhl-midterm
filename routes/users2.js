b01f80c1253667d5711dbb90603f68ea55818be6
Vivian Zhang <vvynz12@gmail.com>
2022-02-17 00:58:50 +0000

    Update users route and redirect a user to their own resource update page

--- routes/users2.js
+++ routes/users2.js
@@ -31,25 +31,22 @@ router.get('/users/:id', (req, res) => {
   // get the resource details from the user's resources
   database.getAllResources(resourceID)
     .then(data => {
-      resParams.user_id = data[0].owner_id;
-      // console.log("USERID:", resParams.user_id);
+      console.log("DATA:", data);
+      resParams.user_id = resourceID;
+      console.log("USERID:", resParams.user_id);
+      resParams.resources = data;
+      console.log("resourceID", data);
     })
+    // .then(() => database.getAllResources(resourceID))
+    // .then(data => {
+    //   // console.log("ALL resources", data);
+    //   resParams.resourceID = data;
+    // })
 
-    .then(() => database.getAllResources(resourceID))
-    .then(data => {
-      // console.log("ALL resources", data);
-      resParams.resourceID = data;
-    })
-
-    .then(() => database.getAllLikedResources(likesResourceID))
-    .then(data => {
-      resParams.name = data[0].name;
-      // console.log(resParams.name);
-    })
     // get all of a user's liked resources
     .then(() => database.getAllLikedResources(likesResourceID))
     .then(data => {
-      // console.log("LIKED", data);
+      console.log("LIKED", data);
       resParams.likesResourceID = data;
     })
 
