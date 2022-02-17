b974da2f353554d10064a0cbf21ed21fc6cc9d42
mrjohnming <john.ngai@hotmail.com>
2022-02-17 21:10:08 +0000

    Update category values to plural form

--- db/seeds/resources.sql
+++ db/seeds/resources.sql
@@ -1,20 +1,38 @@
 INSERT INTO resources (owner_id, title, description, category, url, date_created, date_modified)
-VALUES (4, 'ITS FRIDAY THEN, ITS SATURDAY, SUNDAY! GO MUFASA!', 'TGIF everyone!!', 'video', 'https://www.youtube.com/watch?v=1TewCPi92ro', '2019-12-18', '2020-01-19'),
-(17, 'Flexbox Froggy', 'A game to help you learn CSS Flexbox.', 'tutorial', 'https://flexboxfroggy.com/', '2015-11-25', NULL),
-(13, 'Scarlatti Sonata in E Major, K.380', 'Peformed by classical concert pianist, Tiffany Poon.', 'video', 'https://www.youtube.com/watch?v=EcmFCpoTKXc', '2019-12-13', NULL),
-(18, 'I spent my vacation programming', 'I was just on vacation for four weeks and I spent the entire time writing code.', 'blog', 'https://codeutopia.net/blog/2017/07/11/i-spent-my-vacation-programming/', '2017-03-14', NULL),
-(3, 'Music to put you in a better mood ~ lofi / relax / stress relief', 'Little Soul on Youtube', 'video', 'https://www.youtube.com/watch?v=NiOxSuEJaTI', '2021-07-19', NULL),
-(11, 'Tessa Virtue and Scott Moir''s Moulin Rouge at PyeongChang 2018', 'Tessa and Scott have had an extraordinary career as the most decorated figure skaters of all time', 'video', 'https://www.youtube.com/watch?v=wOEKdWrtz6U', '2018-03-05', NULL),
-(5, 'SQL Zoo', 'Learn SQL in stages.', 'tutorial', 'https://sqlzoo.net/wiki/SQL_Tutorial', '2021-11-23', NULL),
-(16, 'PostgreSQL vs MySQL: Everything You Need to Know', 'A comparative piece on the similarities and differences of two SQL relational database systems.', 'blog', 'https://hackr.io/blog/postgresql-vs-mysql', '2022-02-08', NULL),
-(1, 'How to Learn Git? A Complete Beginners Guide', 'A blog post outlining the benefits of using Git.', 'blog', 'https://hackr.io/blog/how-to-learn-git', '2022-02-11', NULL),
-(5, 'Margin vs Padding in HTML and CSS: Differences and How to Use', 'A blog post comparing the uses of margin and padding in CSS.', 'blog', 'https://hackr.io/blog/margin-vs-padding', '2022-01-27', NULL),
-(4, 'Tomorrowland Belgium 2019 | Official Aftermovie', 'The last Tomorrowland before the world changed :(', 'video', 'https://www.youtube.com/watch?v=hvIg3PTJWxs', '2019-08-09', NULL),
+VALUES (4, 'ITS FRIDAY THEN, ITS SATURDAY, SUNDAY! GO MUFASA!', 'TGIF everyone!!', 'videos', 'https://www.youtube.com/watch?v=1TewCPi92ro', '2019-12-18', '2020-01-19'),
+
+(17, 'Flexbox Froggy', 'A game to help you learn CSS Flexbox.', 'tutorials', 'https://flexboxfroggy.com/', '2015-11-25', NULL),
+
+(13, 'Scarlatti Sonata in E Major, K.380', 'Peformed by classical concert pianist, Tiffany Poon.', 'videos', 'https://www.youtube.com/watch?v=EcmFCpoTKXc', '2019-12-13', NULL),
+
+(18, 'I spent my vacation programming', 'I was just on vacation for four weeks and I spent the entire time writing code.', 'blogs', 'https://codeutopia.net/blog/2017/07/11/i-spent-my-vacation-programming/', '2017-03-14', NULL),
+
+(3, 'Music to put you in a better mood ~ lofi / relax / stress relief', 'Little Soul on Youtube', 'videos', 'https://www.youtube.com/watch?v=NiOxSuEJaTI', '2021-07-19', NULL),
+
+(11, 'Tessa Virtue and Scott Moir''s Moulin Rouge at PyeongChang 2018', 'Tessa and Scott have had an extraordinary career as the most decorated figure skaters of all time', 'videos', 'https://www.youtube.com/watch?v=wOEKdWrtz6U', '2018-03-05', NULL),
+
+(5, 'SQL Zoo', 'Learn SQL in stages.', 'tutorials', 'https://sqlzoo.net/wiki/SQL_Tutorial', '2021-11-23', NULL),
+
+(16, 'PostgreSQL vs MySQL: Everything You Need to Know', 'A comparative piece on the similarities and differences of two SQL relational database systems.', 'blogs', 'https://hackr.io/blog/postgresql-vs-mysql', '2022-02-08', NULL),
+
+(1, 'How to Learn Git? A Complete Beginners Guide', 'A blog post outlining the benefits of using Git.', 'blogs', 'https://hackr.io/blog/how-to-learn-git', '2022-02-11', NULL),
+
+(5, 'Margin vs Padding in HTML and CSS: Differences and How to Use', 'A blog post comparing the uses of margin and padding in CSS.', 'blogs', 'https://hackr.io/blog/margin-vs-padding', '2022-01-27', NULL),
+
+(4, 'Tomorrowland Belgium 2019 | Official Aftermovie', 'The last Tomorrowland before the world changed :(', 'videos', 'https://www.youtube.com/watch?v=hvIg3PTJWxs', '2019-08-09', NULL),
+
 (12, 'Should You Be Taking Melatonin?', 'Should you really be taking melatonin to help you sleep? Experts weigh in on potential risks', 'news', 'https://ca.style.yahoo.com/melatonin-and-sleep-expert-advice-142702305.html', '2022-02-15', NULL),
+
 (7, '''The Hazel McCallion Line:'' Ontario government to name new LRT after former Mississauga mayor', 'The Ontario government has announced that the currently under construction Hurontario LRT will be named ''The Hazel McCallion Line'' in honour of the long-time Mississauga mayor, who turns 101 years old today.', 'news', 'https://www.cp24.com/news/the-hazel-mccallion-line-ontario-government-to-name-new-lrt-after-former-mississauga-mayor-1.5780685', '2022-02-14', NULL),
+
 (3, 'Bloomberg: Where to Invest $100,000 Right Now', 'Bloomberg turned to five wealth managers to find out what areas of the markets those with $100,000 to invest might focus on now. Ideas ranged from betting on undervalued stocks in Europe to cryptocurrency and into the world of private real estate investment trusts.', 'news', 'https://www.bloomberg.com/features/how-to-invest-100k/?srnd=premium-canada', '2020-11-17', '2022-02-15'),
+
 (6, 'SpaceX''s Plans to Send Thousands More Satellites Into Orbit Worry NASA', 'Elon Musk-led company, Boeing, Amazon and others plan fleets of broadband satellites, prompting concerns about space congestion', 'news', 'https://www.wsj.com/articles/spacexs-plans-to-send-thousands-more-satellites-into-orbit-worry-nasa-11644921000?mod=tech_lead_pos7', '2022-02-15', NULL),
-(12, 'CRUISE SHIP Harmony Of The Seas - Full Documentary', 'With its length of 363 meters, width of 66 meters and height of 72 meters, the Harmony Of The Seas is one of the largest cruise ships in the world. The 120,000 ton giant has space for more than 6,300 passengers who can enjoy themselves on the longest slide at sea or in one of two surf pools, among other things. The report accompanies the luxury liner on its maiden voyage.', 'video', 'https://www.youtube.com/watch?v=gaG8FhYf_rc', '2021-01-17', NULL),
-(12, 'Meeting The Amish - First Impressions', 'Interesting perspectives', 'video', 'https://youtu.be/z6SlJZk5guI', '2021-02-28', NULL),
-(15, 'freeCodeCamp: Responsive Web Design', 'Learn the languages that developers use to build webpages: HTML (Hypertext Markup Language) for content, and CSS (Cascading Style Sheets) for design.', 'tutorial', 'https://www.freecodecamp.org/learn/responsive-web-design/', '2014-11-18', NULL),
-(14, 'Eloquent JavaScript: Functions', 'Functions are the bread and butter of JavaScript programming.', 'tutorial', 'https://eloquentjavascript.net/03_functions.html', '2018-12-04', NULL);
+
+(12, 'CRUISE SHIP Harmony Of The Seas - Full Documentary', 'With its length of 363 meters, width of 66 meters and height of 72 meters, the Harmony Of The Seas is one of the largest cruise ships in the world. The 120,000 ton giant has space for more than 6,300 passengers who can enjoy themselves on the longest slide at sea or in one of two surf pools, among other things. The report accompanies the luxury liner on its maiden voyage.', 'videos', 'https://www.youtube.com/watch?v=gaG8FhYf_rc', '2021-01-17', NULL),
+
+(12, 'Meeting The Amish - First Impressions', 'Interesting perspectives', 'videos', 'https://youtu.be/z6SlJZk5guI', '2021-02-28', NULL),
+
+(15, 'freeCodeCamp: Responsive Web Design', 'Learn the languages that developers use to build webpages: HTML (Hypertext Markup Language) for content, and CSS (Cascading Style Sheets) for design.', 'tutorials', 'https://www.freecodecamp.org/learn/responsive-web-design/', '2014-11-18', NULL),
+
+(14, 'Eloquent JavaScript: Functions', 'Functions are the bread and butter of JavaScript programming.', 'tutorials', 'https://eloquentjavascript.net/03_functions.html', '2018-12-04', NULL);
